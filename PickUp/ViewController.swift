524f9b80bf050ed410a43c7a59d092095770c673
kavyan <knkavyanagaraj@gmail.com>
2017-05-18 15:13:10 -0700

    Adding GET and POST data to REST API functionality

--- PickUp/ViewController.swift
+++ PickUp/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        postData()
+        getData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +22,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getData(){
+        let url : String = "http://50.112.71.129/users/"
+        let urlRequest = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlRequest!, completionHandler: {
+            (data, response, error) in
+            if(error != nil){
+                print(error.debugDescription)
+            }
+            else{
+                do{
+                    let  data1 = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                    print(data1)
+                    
+                } catch{
+                        print(error)
+                }
+            }
+            
+        }).resume()
+    }
+    
+    func postData(){
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let parameters = ["first_name": "Bob", "last_name": "Bill", "password": "password123", "email": "bob@bill.com" ] as Dictionary<String, String>
+        
+        //create the url with URL
+        let url = URL(string: "http://50.112.71.129/users/")!
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+    }
 }
+
 
